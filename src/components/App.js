2e2f896a46cb183090abb1d559e3bd42f8f63c92
dariavolyn <75863158+dariavolyn@users.noreply.github.com>
2022-03-20 20:28:31 -0400

    Impact section added

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Intro from './Intro';
 import Problem from './Problem';
 import Solution from './Solution';
 import HowItWorks from './HowItWorks';
+import Impact from './Impact';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Problem />
       <Solution />
       <HowItWorks />
+      <Impact />
     </div>
   );
 }
---
+++ src/components/Impact.js
@@ -0,0 +1,139 @@
+import React from 'react';
+
+function Impact() {
+    const fontColor = '#FF9820';
+
+    return (
+        <div className='impact'>
+            <h2 className='impact__title'>Impact</h2>
+
+            <div className='impact__cards'>
+                <div className='impact__card impact__card_type_business'>
+                    <h3 className='impact__card-title'>For Businesses</h3>
+                    <ul className='impact__card-list'>
+                        <li className='impact__card-text'>Save at least 50% on supply cost</li>
+                        <li className='impact__card-text'>Save 50-60 hours per week of manager’s labor cost: no inventory management, no reordering, no restocking</li>
+                        <li className='impact__card-text'>Save real estate cost for storage</li>
+                        <li className='impact__card-text'>Government helps Subsidize Up to 70%</li>
+                    </ul>
+                </div>
+
+                <div className='impact__card impact__card_type_users'>
+                    <h3 className='impact__card-title'>For Users</h3>
+                    <ul className='impact__card-list'>
+                        <li className='impact__card-text'>Remove all chemicals going into food: plastics, BPA, PFAS and other ‘forever chemicals’ used to hold compostable packaging together</li>
+                        <li className='impact__card-text'>Gamification: reward for returned containers</li>
+                        <li className='impact__card-text'>Sustainability sells: users feel good about helping the planet</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className='impact__table'>
+                <h3 className='impact__table-title'>The competition is no match</h3>
+                <div className='impact__grid'>
+                    <div className='impact__cell'>
+                    </div>
+                    <div className='impact__cell'>
+                        <p className='impact__cell-text impact__cell-text_type_header'>
+                            <span style={{ color: fontColor }}>Awesome Container Company</span>
+                        </p>
+                    </div>
+                    <div className='impact__cell'>
+                        <p className='impact__cell-text impact__cell-text_type_header'>Deliver Zero</p>
+                    </div>
+                    <div className='impact__cell'>
+                        <p className='impact__cell-text impact__cell-text_type_header'>Logistics Company</p>
+                    </div>
+                    <div className='impact__cell'>
+                        <p className='impact__cell-text impact__cell-text_type_header'>Washing Facility</p>
+                    </div>
+                    <div className='impact__cell'>
+                        <p className='impact__cell-text impact__cell-text_type_header'>Single-use plastics</p>
+                    </div>
+
+                    <div className='impact__cell'>
+                        <p className='impact__cell-text impact__cell-text_type_row'>Save over 50% on supply costs</p>
+                    </div>
+                    <div className='impact__cell'>
+                        <div className='impact__cell-circle impact__cell-circle_orange'></div>
+                    </div>
+                    <div className='impact__cell'></div>
+                    <div className='impact__cell'></div>
+                    <div className='impact__cell'></div>
+                    <div className='impact__cell'></div>
+
+                    <div className='impact__cell'>
+                        <p className='impact__cell-text impact__cell-text_type_row'>Save real estate storage costs</p>
+                    </div>
+                    <div className='impact__cell'>
+                        <div className='impact__cell-circle impact__cell-circle_orange'></div>
+                    </div>
+                    <div className='impact__cell'>
+                        <div className='impact__cell-circle impact__cell-circle_black'></div>
+                    </div>
+                    <div className='impact__cell'></div>
+                    <div className='impact__cell'></div>
+                    <div className='impact__cell'></div>
+
+                    <div className='impact__cell'>
+                        <p className='impact__cell-text impact__cell-text_type_row'>Inventory management</p>
+                    </div>
+                    <div className='impact__cell'>
+                        <div className='impact__cell-circle impact__cell-circle_orange'></div>
+                    </div>
+                    <div className='impact__cell'></div>
+                    <div className='impact__cell'></div>
+                    <div className='impact__cell'></div>
+                    <div className='impact__cell'></div>
+
+                    <div className='impact__cell'>
+                        <p className='impact__cell-text impact__cell-text_type_row'>Never reorder supplies</p>
+                    </div>
+                    <div className='impact__cell'>
+                        <div className='impact__cell-circle impact__cell-circle_orange'></div>
+                    </div>
+                    <div className='impact__cell'></div>
+                    <div className='impact__cell'></div>
+                    <div className='impact__cell'></div>
+                    <div className='impact__cell'></div>
+
+                    <div className='impact__cell'>
+                        <p className='impact__cell-text impact__cell-text_type_row'>Easy pickup and delivery</p>
+                    </div>
+                    <div className='impact__cell'>
+                        <div className='impact__cell-circle impact__cell-circle_orange'></div>
+                    </div>
+                    <div className='impact__cell'></div>
+                    <div className='impact__cell'><div className='impact__cell-circle impact__cell-circle_black'></div></div>
+                    <div className='impact__cell'><div className='impact__cell-circle impact__cell-circle_black'></div></div>
+                    <div className='impact__cell'><div className='impact__cell-circle impact__cell-circle_black'></div></div>
+
+                    <div className='impact__cell'>
+                        <p className='impact__cell-text impact__cell-text_type_row'>Sustainable products</p>
+                    </div>
+                    <div className='impact__cell'>
+                        <div className='impact__cell-circle impact__cell-circle_orange'></div>
+                    </div>
+                    <div className='impact__cell'></div>
+                    <div className='impact__cell'></div>
+                    <div className='impact__cell'><div className='impact__cell-circle impact__cell-circle_black'></div></div>
+                    <div className='impact__cell'></div>
+
+                    <div className='impact__cell'>
+                        <p className='impact__cell-text impact__cell-text_type_row'>App for tracking and reporting</p>
+                    </div>
+                    <div className='impact__cell'>
+                        <div className='impact__cell-circle impact__cell-circle_orange'></div>
+                    </div>
+                    <div className='impact__cell'></div>
+                    <div className='impact__cell'><div className='impact__cell-circle impact__cell-circle_black'></div></div>
+                    <div className='impact__cell'></div>
+                    <div className='impact__cell'></div>
+
+                </div>
+            </div>
+        </div >
+    )
+}
+
+export default Impact;
